7609ba72429a6a02576dc96459b39da703fb5ce9
Heo GeonYeong <gyheo@icloud.com>
2023-01-20 10:53:57 +0900

    Edit MemberRepository

--- src/main/java/hello/hellospring/repository/MemberRepository.java
+++ src/main/java/hello/hellospring/repository/MemberRepository.java
@@ -9,6 +9,5 @@ public interface MemberRepository {
     Member save(Member member);
     Optional<Member> findById(Long id);
     Optional<Member> findByName(String name);
-
     List<Member> findAll();
 }
